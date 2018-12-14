8270461d67153c626cd1346211a3efb0c7304158
Imre Bartis <imre.bartis@siili.com>
2018-12-14 05:56:26 +0200

    style the search bar

--- video-browser/src/App.vue
+++ video-browser/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container">
     <SearchBar @termChange="onTermChange"></SearchBar>
     <VideoList :videos="videos"></VideoList>
   </div>
@@ -7,35 +7,36 @@
 
 <script>
 import SearchBar from "./components/SearchBar";
-import axios from 'axios';
-import VideoList from './components/VideoList';
+import axios from "axios";
+import VideoList from "./components/VideoList";
 require("dotenv").config({ path: "variables.env" });
 
-const API_KEY = process.env.API_KEY
+const API_KEY = process.env.API_KEY;
 
 export default {
   name: "App",
   components: {
     SearchBar,
-    VideoList,
+    VideoList
   },
   data() {
     return { videos: [] };
   },
   methods: {
     onTermChange(searchTerm) {
-      axios.get('https://www.googleapis.com/youtube/v3/search', {
-        params: {
-          key: API_KEY,
-          type: 'video',
-          part: 'snippet',
-          q: searchTerm,
-        }
-      }).then(response => {
-        this.videos = response.data.items
-      })
+      axios
+        .get("https://www.googleapis.com/youtube/v3/search", {
+          params: {
+            key: API_KEY,
+            type: "video",
+            part: "snippet",
+            q: searchTerm
+          }
+        })
+        .then(response => {
+          this.videos = response.data.items;
+        });
     }
   }
 };
 </script>
-
