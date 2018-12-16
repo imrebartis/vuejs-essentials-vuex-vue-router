f70d5b01b230e8350edebc432fe8b4ca0876e167
Imre Bartis <imre.bartis@siili.com>
2018-12-16 09:47:07 +0100

    add thumbnail images

--- video-browser/src/components/VideoListItem.vue
+++ video-browser/src/components/VideoListItem.vue
@@ -1,11 +1,14 @@
 <template>
-  <li class="list-group-item">{{ video.snippet.title }}</li>
+  <li class="list-group-item">
+    <img :src="video.snippet.thumbnails.default.url">
+    {{ video.snippet.title }}
+  </li>
 </template>
 
 <script>
 export default {
-  name: 'VideoListItem',
-  props: ['video'],
+  name: "VideoListItem",
+  props: ["video"]
 };
 </script>
 
