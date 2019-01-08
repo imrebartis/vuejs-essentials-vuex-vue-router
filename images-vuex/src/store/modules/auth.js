cce1e654cf7f3c1b5410a8f42652ee3c3fb10116
Imre Bartis <imre.bartis@siili.com>
2019-01-08 06:51:28 +0200

    initiate the login flow

--- images-vuex/src/store/modules/auth.js
+++ images-vuex/src/store/modules/auth.js
@@ -1,3 +1,5 @@
+import api from '../../api/imgur';
+
 const state = {
   token: null
 };
@@ -14,6 +16,9 @@ const mutations = {
 };
 
 const actions = {
+  login: () => {
+    api.login();
+  },
   logout: ({ commit }) => {
     commit("setToken", null);
   }
